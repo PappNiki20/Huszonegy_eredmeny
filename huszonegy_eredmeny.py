e59b62c9b7ec5a5797fac3308945dbc0ba24275b
PappNiki20 <papp.nikoletta001@gmail.com>
2023-02-02 13:19:38 +0100

    a vesztesek tesztben hibas feltetelt adtam meg

--- huszonegy_eredmeny.py
+++ huszonegy_eredmeny.py
@@ -13,13 +13,23 @@ def ossz(lista):
     return lapossz
 # Teszt esetek
 
-def jvesztes_ponthuszonegy_teszt():
+# játékos veszt
+def jvesztes_nagyobb_mint_huszonegy_teszt():
     
-   if eredmenyek.jpontszama == 21:
-        print("A jvesztes_ponthuszonegy teszt sikeres")
+   if eredmenyek.jpontszama > 21:
+        print("A jvesztes_nagyobb_mint_huszonegy teszt sikeres")
 
    else:
-        print("A jvesztes_ponthuszonegy sikertelen!")
+        print("A jvesztes_nagyobb_mint_huszonegy teszt sikertelen!")
+
+
+def jvesztes_kevesebb_mint_a_gep_teszt():
+    
+   if eredmenyek.jpontszama < eredmenyek.gpontszama:
+        print("A jvesztes_kevesebb_mint_a_gep teszt sikeres")
+
+   else:
+        print("A jvesztes_kevesebb_mint_a_gep sikertelen!")
 
 
 
@@ -30,3 +40,35 @@ def jvesztes_gepkozelebb_huszonegyhez_teszt():
 
    else:
         print("A jvesztes_gepkozelebb_huszonegyhez sikertelen!")
+
+# gép veszt
+
+def gvesztes_nagyobb_mint_huszonegy_teszt():
+    
+   if eredmenyek.gpontszama > 21:
+        print("A gvesztes_nagyobb_mint_huszonegy teszt sikeres")
+
+   else:
+        print("A gvesztes_nagyobb_mint_huszonegy teszt sikertelen!")
+
+
+def gvesztes_kevesebb_mint_a_jatekos_teszt():
+    
+   if eredmenyek.gpontszama < eredmenyek.jpontszama:
+        print("A gvesztes_ponthuszonegy teszt sikeres")
+
+   else:
+        print("A gvesztes_ponthuszonegy sikertelen!")
+
+
+
+def gvesztes_gepkozelebb_huszonegyhez_teszt():
+    
+   if eredmenyek.jkozelebb < eredmenyek.gkozelebb:
+        print("A gvesztes_gepkozelebb_huszonegyhez teszt sikeres")
+
+   else:
+        print("A gvesztes_gepkozelebb_huszonegyhez sikertelen!")
+
+
+# játékos nyert
