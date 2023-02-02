adc1577e8309b00297b384f7059b9886a3644ece
PappNiki20 <papp.nikoletta001@gmail.com>
2023-02-02 13:07:17 +0100

    ket teszteset a jatekos vesztesehez

--- huszonegy_eredmeny.py
+++ huszonegy_eredmeny.py
@@ -1,4 +1,32 @@
 # megoldas
-def eredmenyek():
-    pass
+def eredmenyek(jlapjai:list, glapjai:list):
+     jpontszama = ossz(jlapjai)
+     gpontszama = ossz(glapjai)
+     gkozelebb = 21-gpontszama
+     jkozelebb = 21-jpontszama
+
+
+def ossz(lista):
+    lapossz = 0
+    for i in lista:
+        lapossz += i
+    return lapossz
 # Teszt esetek
+
+def jvesztes_ponthuszonegy_teszt():
+    
+   if eredmenyek.jpontszama == 21:
+        print("A jvesztes_ponthuszonegy teszt sikeres")
+
+   else:
+        print("A jvesztes_ponthuszonegy sikertelen!")
+
+
+
+def jvesztes_gepkozelebb_huszonegyhez_teszt():
+    
+   if eredmenyek.gkozelebb < eredmenyek.jkozelebb:
+        print("A jvesztes_gepkozelebb_huszonegyhez teszt sikeres")
+
+   else:
+        print("A jvesztes_gepkozelebb_huszonegyhez sikertelen!")
